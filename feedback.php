5777d885d4f0dad524fd8780ba6400272c9e3173
rd <git@demus.at>
2020-07-16 23:27:28 +0200

    open feedback for javascript

--- feedback.php
+++ feedback.php
@@ -1,5 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require_once "config/config.php"; // config db, etc
 require_once "lib/error.php"; // error
@@ -7,7 +9,6 @@ require_once "lib/feedback_data.php"; // feedback data
 
 // mysql
 $db = new mysqli($config['dbhost'], $config['dbuser'], $config['dbpassword'], $config['dbname']);
-
 
 if ($db->connect_error)
 {
@@ -29,14 +30,25 @@ else
 		{
 			echo return_new_token($db);
 		}
-		else if(validate_key($db,$token))
+		else
 		{
-			if(isset($_GET['subject']))
+			// illegal token
+			header($_SERVER["SERVER_PROTOCOL"]." 451 Unavailable For Legal Reasons");
+			echo return_error_unknown_key($token);
+			return;
+		}
+	}
+	else if (isset($_POST['token']))
+	{
+		$token = $db->real_escape_string($_POST['token']);
+		if(validate_key($db,$token))
+		{
+			if(isset($_POST['subject']))
 			{
-				if(isset($_GET['message']))
+				if(isset($_POST['message']))
 				{
-					$subject = $db->real_escape_string($_GET['subject']);
-					$message = $db->real_escape_string($_GET['message']);
+					$subject = $db->real_escape_string($_POST['subject']);
+					$message = $db->real_escape_string($_POST['message']);
 					if(save_feedback($db,$subject,$message,$token))
 					{
 						echo json_encode(array('OK' => array('code' => 'save','info' => 'Message saved',),));
@@ -78,7 +90,7 @@ else
 		header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 		echo return_error_unknown_request();
 		return;
-	}	
+	}
 	$db->close();
 } // $db
 
